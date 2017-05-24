fde90f23f7805fffa8a3c77f4235df836ff0522d
fsuteresa2000 <fsuteresa2000@yahoo.com>
2017-05-24 17:07:15 -0500

    checkpoint-4-listmessages-complete

---
+++ app/scripts/Message.js
@@ -0,0 +1,22 @@
+(function() {
+    function Message($firebaseArray) {
+        var Message = {};
+        var ref = firebase.database().ref().child("messages");
+        var messages = $firebaseArray(ref);
+
+        Message.getByRoomId = function(roomId) {
+            return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+        }
+
+        Message.send = function (newMessage) {
+            messages.$add(newMessage);
+            newMessage.sentAt = firebase.database.ServerValue.TIMESTAMP;
+        }
+
+        return Message;
+    }
+
+    angular
+        .module('blocChat')
+        .factory('Message', ['$firebaseArray', Message]);
+})();
--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,8 @@
 (function() {
-    function HomeCtrl(Room, $uibModal) {
+    function HomeCtrl(Room, Message, $uibModal) {
         this.rooms = Room.all;
+        this.currentRoom = null;
+
         this.addRoom = function() {
             $uibModal.open({
                 templateUrl: '/templates/modal.html',
@@ -8,9 +10,16 @@
                 controller: 'ModalCtrl as modal'
             });
         }
-    }
+
+
+        this.setCurrentRoom = function(room) {
+            this.currentRoom = room;
+            this.messages = Message.getByRoomId(this.currentRoom.$id);
+        }
+
+      }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 })();
